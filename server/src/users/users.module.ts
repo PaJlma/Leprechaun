59953ed2029c62d70e789008cc1125c6c4387cf5
PaJlma <palma21042005@gmail.com>
2023-11-26 14:43:36 +0300

    pluged users files service

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from "./users.service";
 import { UsersController } from "./users.controller";
 import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "./schemas/user.schema";
+import { UsersFilesService } from "./usersFiles.service";
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
-  providers: [UsersService],
+  providers: [UsersService, UsersFilesService],
   controllers: [UsersController],
 })
 export class UsersModule {}
