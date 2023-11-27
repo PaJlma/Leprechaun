588eaaf688b3dc1243b6aefcfcbe40cbd76d4602
PaJlma <palma21042005@gmail.com>
2023-11-27 14:36:00 +0300

    documented CreateUserDto

--- server/src/users/dtos/createUser.dto.ts
+++ server/src/users/dtos/createUser.dto.ts
@@ -1,7 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUserDto {
+  @ApiProperty({
+    description: "Логин пользователя",
+    type: String,
+    required: true,
+  })
   login: string;
 
+  @ApiProperty({
+    description: "Электронный почтовый адрес пользователя",
+    type: String,
+    required: true,
+  })
   email: string;
 
+  @ApiProperty({
+    description: "Пароль пользователя",
+    type: String,
+    required: true,
+  })
   password: string;
 }
