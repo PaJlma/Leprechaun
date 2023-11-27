05d1679be0589efb2a43db81823d0e4225d84e61
PaJlma <palma21042005@gmail.com>
2023-11-27 14:28:00 +0300

    documented create video dto

--- server/src/videos/dtos/createVideo.dto.ts
+++ server/src/videos/dtos/createVideo.dto.ts
@@ -1,5 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateVideoDto {
+  @ApiProperty({
+    description: "Название видео",
+    type: String,
+    required: true,
+  })
   title: string;
 
+  @ApiProperty({
+    description: "ID автора",
+    type: String,
+    required: true,
+  })
   author: string;
 }
