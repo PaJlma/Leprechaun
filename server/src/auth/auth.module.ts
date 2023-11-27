4463da6170503b6a8821b9cd2adad4ba6b187bf2
PaJlma <palma21042005@gmail.com>
2023-11-27 14:10:04 +0300

    added forward ref for VideosModule

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,12 +1,13 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { UsersModule } from "@/users/users.module";
 import { SessionsModule } from "@/sessions/sessions.module";
 import { JwtModule } from "@nestjs/jwt";
+import { VideosModule } from "@/videos/videos.module";
 
 @Module({
-  imports: [JwtModule.register({}), UsersModule, SessionsModule],
+  imports: [JwtModule.register({}), UsersModule, SessionsModule, forwardRef(() => VideosModule)],
   providers: [AuthService],
   controllers: [AuthController],
   exports: [AuthService, JwtModule],
