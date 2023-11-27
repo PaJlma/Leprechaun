0d362dbd67fe604b257d674c92be3de2801995ee
PaJlma <palma21042005@gmail.com>
2023-11-27 14:44:03 +0300

    documented RefreshSessionDto

--- server/src/sessions/dtos/refreshSession.dto.ts
+++ server/src/sessions/dtos/refreshSession.dto.ts
@@ -1,7 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class RefreshSessionDto {
+  @ApiProperty({
+    description: "ID пользователя для которого была создана данная сессия",
+    type: String,
+  })
   user: string;
 
+  @ApiProperty({
+    description: "Уникальный отпечаток клиента (Например для браузеров это user-agent)",
+    type: String,
+  })
   fingerPrint: string;
 
+  @ApiProperty({
+    description: "Refresh токен, действующий для данной сессии",
+    type: String,
+  })
   oldRefreshToken: string;
 }
