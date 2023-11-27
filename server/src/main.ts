3ebde8ab6b5faa0a5eacf56c555d45ccd9e16b0c
PaJlma <palma21042005@gmail.com>
2023-11-27 14:21:33 +0300

    pluged swagger to app

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,23 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
 import { IConfiguration } from "./configuration/configuration.types";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger Configuration
+  const swaggerConfig = new DocumentBuilder()
+  .setTitle("Forest API")
+  .setContact("PaJlma", "https://github.com/PaJlma", "palma21042005@gmail.com")
+  .setDescription("Есть вопросы - <a href=\"https://t.me/PaJlma\">Мой Телеграм</a>")
+  .setVersion("1.0.0")
+  .setLicense("Unlicensed", "")
+  .build()
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup("api/docs", app, swaggerDocument);
+  //
 
   const configService = app.get(ConfigService);
   
