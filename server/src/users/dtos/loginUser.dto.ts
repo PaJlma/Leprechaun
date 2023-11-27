ee5b8115ddf5a24226a0e28f1ab7c65775a441dd
PaJlma <palma21042005@gmail.com>
2023-11-27 14:37:43 +0300

    documented DeleteUserDto

--- server/src/users/dtos/loginUser.dto.ts
+++ server/src/users/dtos/loginUser.dto.ts
@@ -1,5 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class LoginUserDto {
+  @ApiProperty({
+    description: "Электронный почтовый адрес пользователя",
+    type: String,
+    required: true,
+  })
   email: string;
 
+  @ApiProperty({
+    description: "Пароль пользователя",
+    type: String,
+    required: true,
+  })
   password: string;
 }
