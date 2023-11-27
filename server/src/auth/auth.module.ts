9fa14fc536f2c8940b6cdda9e90a1e1be6d8dba1
PaJlma <palma21042005@gmail.com>
2023-11-27 11:11:03 +0300

    pluged modules

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
+import { UsersModule } from "@/users/users.module";
+import { SessionsModule } from "@/sessions/sessions.module";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
+  imports: [JwtModule.register({}), UsersModule, SessionsModule],
   providers: [AuthService],
   controllers: [AuthController],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
