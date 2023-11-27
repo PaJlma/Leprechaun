f403e0ad27ff12c8046d850dc48180f9837c5541
PaJlma <palma21042005@gmail.com>
2023-11-27 14:09:42 +0300

    added forward ref for UsersModule and pluged videos schema

--- server/src/videos/videos.module.ts
+++ server/src/videos/videos.module.ts
@@ -1,9 +1,19 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { VideosService } from "./videos.service";
 import { VideosController } from "./videos.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Video, VideoSchema } from "./schemas/video.schema";
+import { AuthModule } from "@/auth/auth.module";
+import { VideosFilesService } from "./videosFiles.service";
+import { UsersModule } from "@/users/users.module";
 
 @Module({
-  providers: [VideosService],
+  imports: [
+    MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }]),
+    forwardRef(() => AuthModule),
+    UsersModule,
+  ],
+  providers: [VideosService, VideosFilesService],
   controllers: [VideosController],
   exports: [VideosService],
 })
