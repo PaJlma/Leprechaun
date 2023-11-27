a126c5f20ccb32a26de1601372df7a3423e3c4a1
PaJlma <palma21042005@gmail.com>
2023-11-27 14:42:28 +0300

    documented CreateSessionDto

--- server/src/sessions/dtos/createSession.dto.ts
+++ server/src/sessions/dtos/createSession.dto.ts
@@ -1,9 +1,27 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateSessionDto {
+  @ApiProperty({
+    description: "ID пользователя для которого была создана данная сессия",
+    type: String,
+  })
   user: string;
 
+  @ApiProperty({
+    description: "Уникальный отпечаток клиента (Например для браузеров это user-agent)",
+    type: String,
+  })
   fingerPrint: string;
 
+  @ApiProperty({
+    description: "Access токен, действующий для данной сессии",
+    type: String,
+  })
   access: string;
 
+  @ApiProperty({
+    description: "Refresh токен, действующий для данной сессии",
+    type: String,
+  })
   refresh: string;
 }
