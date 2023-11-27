56d5c101d6402cd5fe24f0433e07cd113c69f860
PaJlma <palma21042005@gmail.com>
2023-11-27 14:43:07 +0300

    documented DeleteSessionDto

--- server/src/sessions/dtos/deleteSession.dto.ts
+++ server/src/sessions/dtos/deleteSession.dto.ts
@@ -1,5 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class DeleteSessionDto {
+  @ApiProperty({
+    description: "ID пользователя для которого была создана данная сессия",
+    type: String,
+  })
   user: string;
 
+  @ApiProperty({
+    description: "Уникальный отпечаток клиента (Например для браузеров это user-agent)",
+    type: String,
+  })
   fingerPrint: string;
 }
