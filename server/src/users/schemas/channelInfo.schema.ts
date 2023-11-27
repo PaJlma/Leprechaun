d0a323a690d0f2980ad935065b08fed46c0afd77
PaJlma <palma21042005@gmail.com>
2023-11-27 14:35:00 +0300

    documented channelInfo schema

--- server/src/users/schemas/channelInfo.schema.ts
+++ server/src/users/schemas/channelInfo.schema.ts
@@ -1,13 +1,22 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { ApiProperty } from "@nestjs/swagger";
 import { HydratedDocument } from "mongoose";
 
 export type ChannelInfoDocument = HydratedDocument<ChannelInfo>;
 
 @Schema()
 export class ChannelInfo {
+  @ApiProperty({
+    description: "Описание канала пользователя",
+    type: String,
+  })
   @Prop()
   description: string;
 
+  @ApiProperty({
+    description: "Состояние подтверждения канала пользователя",
+    type: String,
+  })
   @Prop()
   isConfirmed: boolean;
 }
