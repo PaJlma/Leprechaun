a53263e3386b129df50d3257608972d3d3272472
PaJlma <palma21042005@gmail.com>
2023-11-27 14:26:52 +0300

    documented videoDuration schema

--- server/src/videos/schemas/videoDuration.schema.ts
+++ server/src/videos/schemas/videoDuration.schema.ts
@@ -1,16 +1,29 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { ApiProperty } from "@nestjs/swagger";
 import { HydratedDocument } from "mongoose";
 
 export type VideoDurationDocument = HydratedDocument<VideoDuration>;
 
 @Schema()
 export class VideoDuration {
+  @ApiProperty({
+    description: "Длина видео в часах",
+    type: Number,
+  })
   @Prop()
   hours: number;
 
+  @ApiProperty({
+    description: "Длина видео в минутах",
+    type: Number,
+  })
   @Prop()
   minutes: number;
 
+  @ApiProperty({
+    description: "Длина видео в секундах",
+    type: Number,
+  })
   @Prop()
   seconds: number;
 }
