22b3820d22176070d16d5f8d6c9630a2c6f883fc
PaJlma <palma21042005@gmail.com>
2023-11-28 09:42:05 +0300

    validated Create User Dto

--- server/src/users/dtos/createUser.dto.ts
+++ server/src/users/dtos/createUser.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 
 export class CreateUserDto {
   @ApiProperty({
@@ -6,6 +7,8 @@ export class CreateUserDto {
     type: String,
     required: true,
   })
+  @IsNotEmpty()
+  @IsString()
   login: string;
 
   @ApiProperty({
@@ -13,6 +16,8 @@ export class CreateUserDto {
     type: String,
     required: true,
   })
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 
   @ApiProperty({
@@ -20,5 +25,9 @@ export class CreateUserDto {
     type: String,
     required: true,
   })
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  @MaxLength(40)
   password: string;
 }
