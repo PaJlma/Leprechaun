4cc1cd8788405bf33a89f93b1cf28d376b5727c2
PaJlma <palma21042005@gmail.com>
2023-11-28 09:34:01 +0300

    documented videos controller

--- server/src/videos/videos.controller.ts
+++ server/src/videos/videos.controller.ts
@@ -4,6 +4,7 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
   Post,
   Req,
@@ -19,16 +20,28 @@ import { AuthGuard } from "@/auth/auth.guard";
 import { AuthService } from "@/auth/auth.service";
 import { Request } from "express";
 import { IPreparedVideo } from "./types/preparedVideo.interface";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
+@ApiTags("Операции с видео")
 @Controller("videos")
 export class VideosController {
   constructor(private readonly videosService: VideosService) {}
 
+  @ApiOperation({ summary: "Получение всех видео" })
+  @ApiResponse({
+    type: Video,
+    status: HttpStatus.OK,
+  })
   @Get()
   getPreparedVideos(): Promise<IPreparedVideo[]> {
     return this.videosService.getPreparedVideos();
   }
 
+  @ApiOperation({ summary: "Создание видео" })
+  @ApiResponse({
+    type: Video,
+    status: HttpStatus.CREATED,
+  })
   @Post()
   @UseGuards(AuthGuard)
   @UseInterceptors(
@@ -55,6 +68,11 @@ export class VideosController {
     return this.videosService.create(dto, video[0], preview[0]);
   }
 
+  @ApiOperation({ summary: "Удаление видео" })
+  @ApiResponse({
+    type: Video,
+    status: HttpStatus.OK,
+  })
   @Delete("/:videoId")
   @UseGuards(AuthGuard)
   async delete(@Param("videoId") videoId: string, @Req() request: Request): Promise<Video> {
