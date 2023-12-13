5fe9a3dde7b410590f1c1e0b62bf7455a5a62973
PaJlma <palma21042005@gmail.com>
2023-12-13 20:37:10 +0300

    pluged svgr module and made path allias in vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svgr({
+      include: "**/*.svg?react",
+    }),
+  ],
+
+  server: {
+    port: 3000,
+  },
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
